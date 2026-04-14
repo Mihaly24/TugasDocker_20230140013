cd56e7302ead6bee4fcfe67c3d45c816d94fe7a6
Mihaly24 <fairuzk20@gmail.com>
2026-04-14 13:43:51 +0700

    Initial commit

---
+++ src/test/java/com/deploy/tugas/TugasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.tugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
